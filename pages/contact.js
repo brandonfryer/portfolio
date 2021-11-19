bd4930d619e3dffe5e6f588ac41bf958c98da497
brandonfryer <fryer.brandon@gmail.com>
2021-11-18 23:28:38 -0800

    alphabetized html attributes. started adding some form validation functionality. not finished

--- pages/contact.js
+++ pages/contact.js
@@ -1,33 +1,48 @@
+import React, { useEffect, useState, useRef } from 'react';
 import styles from '../styles/Contact.module.css'
 
 export default function Contact() {
+    const [name, setName] = useState('');
+    const nameInputRef = useRef(null);
+
+    useEffect(() => {
+        console.log(nameInputRef)
+    }, [nameInputRef.current]);
+
+    useEffect(() => {
+        console.log(name)
+    }, [name])
+
     return (
         <section className={styles.section}>
             <form className={styles.form}>
                 <label className={styles.label}>Name</label>
                 <input
-                    type="text"
-                    name="name"
                     className={styles.name}
                     id="inputName"
-                    onInput="checkInput()"
+                    name="name"
+                    onChange={(e) => setName(e.target.value)}
+                    ref={nameInputRef}
+                    type="text"
                 />
                 <label className={styles.label}>Mail</label>
                 <input
-                    type="text"
-                    name="email"
                     className={styles.email}
                     id="inputMail"
-                    onInput="checkInput()"
+                    name="email"
+                    onChange={(e) => setName(e.target.value)}
+                    ref={nameInputRef}
+                    type="text"
                 />
                 <label className={styles.label}>Your Message</label>
                 <textarea
-                    name="message"
-                    cols="30"
-                    rows="10"
                     className={styles.message}
+                    cols="30"
                     id="inputMessage"
-                    onInput="checkInput()"
+                    name="message"
+                    onChange={(e) => setName(e.target.value)}
+                    ref={nameInputRef}
+                    rows="10"
                 ></textarea>
                 <button type="submit" className={styles.button} disabled>
                     Submit
